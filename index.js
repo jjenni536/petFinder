06e0f591c939ec052bb85c242eff593b26c1bb15
justice jennings <justice.l.jennings@gmail.com>
2023-12-12 17:54:18 -0500

    corrected index.js code to properly get endpoints.

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-// define API source
- const baseApiUrl = '/api/v1/pets'
+// require express so it can be used
+const express = require('express');
 
-// create function to fetch data from API.
-const getAllPets = async () => {
-    // place try/catch for proper error-handling. (just incase)
-    try {
-      // Call api to get ALL pets
-      const allPets = await response.getAllPets();
-      // Return the list of ALL pets
-      return allPets;
-      // Handle error
-    } catch (error) {
-      console.error("Error fetching all pets:", error);
-    }
-  }
+const app = express();
+
+//specify port for it to run on
+const PORT = 3000;
+
+// input filler data for testing
+const pets = [
+  { id: 1, name: 'Mance', owner: 'Jireh' },
+  { id: 2, name: 'Sharay', owner: 'iAm' },
+  { id: 2, name: 'Tiffany', owner: 'Allah' },
+  { id: 2, name: 'RuJay', owner: 'jehovah' },
+
+];
+
+// create dndpoint to get all pets
+app.get('/pets', (req, res) => {
+  res.json(pets);
+});
+
+// create ndpoint to get pets by name
+app.get('/pets/:name', (req, res) => {
+  const petName = req.params.name;
+  const filteredPets = pets.filter((pet) => pet.name === petName);
+  res.json(filteredPets);
+});
+
+// create endpoint to get pets by owner's name
+app.get('/pets/owner/:ownerName', (req, res) => {
+  const ownerName = req.params.ownerName;
+  const filteredPets = pets.filter((pet) => pet.owner === ownerName);
+  res.json(filteredPets);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
